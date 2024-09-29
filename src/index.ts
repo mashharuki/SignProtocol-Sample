3b634ed1ebacb00274f95af6abe0aab8bdb47e68
Haruki Kondo <avp.104.106.107@ae.auone-net.jp>
2024-09-29 15:02:06 +0000

    追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 import { SignProtocolClient, SpMode, EvmChains } from "@ethsign/sp-sdk";
 import { privateKeyToAccount } from "viem/accounts";
+import { BigNumber, ethers } from "ethers";
 
 // 秘密鍵を環境変数からimportする
 dotenv.config();
@@ -9,13 +10,15 @@ const {
   PRIVATE_KEY
 } = process.env;
 
+const account = privateKeyToAccount(PRIVATE_KEY as `0x${string}`);
+
 /**
  * create SignProtocol client instance
  */
 const createSignProtocolClient = () => {
   return new SignProtocolClient(SpMode.OnChain, {
     chain: EvmChains.baseSepolia,
-    account: privateKeyToAccount(PRIVATE_KEY as `0x${string}`), // Optional, depending on environment
+    account: account 
   });
 }
 
